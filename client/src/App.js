4e88f4ac351db2ea8d04917e719c5a7d7effa244
danielcaicedo98 <daniel.andres.caicedo@correounivalle.edu.co>
2022-05-12 14:09:10 -0500

    upload

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react';
+import './App.css';
+
+//Components
+
+import InputTodo from './components/InputTodos';
+
+function App() {
+  return <Fragment>
+    <InputTodo/>
+  </Fragment>
+}
+
+export default App;
---
+++ client/src/components/InputTodos.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+
+const InputTodo = () =>{
+    return (
+        <Fragment>
+            <h1 className="text-center mt-5">Pern Todo List</h1>
+            <form className="d-flex">
+                <input type="text" className="form-control" />
+                <button className="btn btn-success">Add</button>
+            </form>
+        </Fragment>
+    );
+};  
+export default InputTodo;
